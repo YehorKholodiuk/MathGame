ecf81da50c1491272ffb01a26d9c9b01bf77e017
ASUS <Yehor Kholodiuk>
2022-06-22 15:11:57 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+const operandVariants = ['+','-','*']
+
+function random(min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getRandomOperand = () => operandVariants[random(0,2)]
+const randomNumber = () => random(0,10)
+const calculate = (n1,n2,operand) => {
+
+  if (operand === '+') return n1 + n2;
+  if (operand === '-') return n1 - n2;
+  if (operand ==='*')  return  n1 * n2;
+};
 
 function App() {
+
+  const n1Initial = randomNumber()
+  const n2Initial = randomNumber()
+  const operandInitial = getRandomOperand()
+
+  const [operand, setOperand] = useState(operandInitial)
+  const [n1, setN1] = useState(n1Initial)
+  const [n2, setN2] = useState(n2Initial)
+  const [result, setResult] = useState(calculate(n1Initial,n2Initial,operandInitial))
+
+  const shake = () => {
+    const n1 = randomNumber(),  n2 = randomNumber(), operand = getRandomOperand()
+
+    setN1(n1)
+    setN2(n2)
+    setOperand(operand)
+    setResult(calculate(n1,n2,operand))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {n1} {operand} {n2} = {''} {' '}
+    <hr/>
+      <button onClick={shake} >Shake</button>
+      <hr/>
+      {result}
     </div>
   );
 }
